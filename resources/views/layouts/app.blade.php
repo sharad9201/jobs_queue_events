44a317e29865db6c5816c7c1cb0d400bb081ca5b
sharad9201 <sharad.rae19@gmail.com>
2021-08-20 22:01:29 +0545

    core ui setup and main body design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,4 +1,26 @@
 <!doctype html>
+<html lang="en">
+ <head>
+ <!-- Required meta tags -->
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+
+ <!-- CoreUI CSS -->
+ <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
+
+ <title>{{ config('app.name', 'Laravel') }}</title>
+
+</head>
+ <body class="c-app">
+    @yield('content')
+ <!-- Optional JavaScript -->
+ <!-- Popper.js first, then CoreUI JS -->
+ <script src="https://unpkg.com/@popperjs/core@2"></script>
+ <script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
+ </body>
+</html>
+
+{{-- <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -80,4 +102,4 @@
         </main>
     </div>
 </body>
-</html>
+</html> --}}
